1acdf4ae840e05fc970440b535542dcbf4c3af16
Artem <Kolesnikov>
2012-10-14 00:09:35 +0700

    greed.columns, greed.cursor

--- jquery.greed.js
+++ jquery.greed.js
@@ -6,6 +6,8 @@
      */
     var Greed = {};
 
+    Greed.instances = [];
+
     /**
      * Greed options
      *
@@ -13,7 +15,6 @@
      * @param <object> options
      */
     Greed.options = function(greed, options){
-
         /**
          * Greed object
          */
@@ -37,7 +38,10 @@
          */
         this.get = function(name){
             if(name != undefined){
-                return options[name];
+                if(typeof(options[name]) != 'undefined'){
+                    return options[name];
+                }
+                return null;
             }
             return options;
         }
@@ -54,6 +58,107 @@
             }
         }
     };
+
+    /**
+     * Table columns
+     */
+    Greed.columns = function(greed){
+        /**
+         * Greed object
+         */
+        var greed = greed;
+
+        /**
+         * Column names
+         */
+        var names = [];
+
+        /**
+         * Column types
+         */
+        var types = [];
+
+        /**
+         * Column captions
+         */
+        var captions = [];
+
+        /**
+         * Fill columns data
+         */
+        var columnsData = greed.options.get('columns');
+        if(columnsData != null){
+            for(var columnName in columnsData){
+                if(typeof(columnsData[columnName]) == 'object'){
+                    names.push(columnName);
+                    types.push(typeof(columnsData[columnName].type) != 'undefined' ? columnsData[columnName].type : 'string');
+                    captions.push(typeof(columnsData[columnName].caption) != 'undefined' ? columnsData[columnName].caption : '');
+                }
+            }
+        }
+
+        /**
+         * Get column caption
+         */
+        this.getCaption = function(idx){
+            return typeof(captions[idx]) != 'undefined' ? captions[idx] : '';
+        }
+    };
+
+    Greed.cursor = function(greed){
+        /**
+         * Greed object
+         */
+        var greed = greed;
+
+        /**
+         * Cursor DIV object
+         */
+        var div = null;
+
+        /**
+         * Is cursor active
+         */
+        var active = false;
+
+        /**
+         * Cursor position
+         */
+        var position = {
+            col: 0,
+            row: 0
+        };
+
+        this.init = function(){
+            div = $('<DIV>');
+            div.addClass('cursor');
+            greed.container.append(div);
+        }
+
+
+        this.place = function(col, row){
+            position.col = col;
+            position.row = row;
+            active = true;
+            this.redraw();
+        }
+
+        this.redraw = function(){
+            var curCell = greed.table.getCell(position.col, position.row);
+            var pos = curCell.offset();
+            var tpos = greed.container.offset();
+            div.css('top', pos.top - tpos.top);
+            div.css('left', pos.left - tpos.left);
+            div.width(curCell.outerWidth() - 4);
+            div.height(curCell.outerHeight() - 4);
+            div.show();
+        }
+
+        this.hide = function(){
+            active = false;
+            div.hide();
+        }
+    }
 
     /**
      * Greed table object
@@ -100,11 +205,52 @@
          */
         this.createTable = function(){
             table = $('<TABLE>');
+            table.attr('cellpadding', 0);
+            table.attr('cellspacing', 0);
             table.addClass('datagrid');
             greed.container.append(table);
+
+            table.bind('selectstart', function(e){
+                e.preventDefault();
+            });
+            if(greed.options.get('showColumnCaptions')){
+                addColumnCaptionsRow();
+            }
             for(var row=0; row < rows; row++){
                 this.addRow(data[row]);
             }
+            table.click(function(_greed){
+                return function(e){
+                    if(typeof(e.target) != 'undefined' && e.target && e.target.nodeName == 'TD'){
+                        for(var i=0; i<Greed.instances.length; i++){
+                            Greed.instances[i].cursor.hide();
+                        }
+
+                        var col = e.target.cellIndex;
+                        var row = e.target.parentNode.rowIndex;
+                        if(_greed.options.get('showRowNumbers')){
+                            col--;
+                        }
+                        if(_greed.options.get('showColumnCaptions')){
+                            row--;
+                        }
+                        _greed.cursor.place(col, row);
+                        e.preventDefault();
+                    }
+                }
+            }(greed));
+
+            $(window).keydown(function(e){
+               console.log(e);
+            });
+        }
+
+        /**
+         * Redraws the table
+         */
+        this.redraw = function(){
+            table.remove();
+            this.createTable();
         }
 
         /**
@@ -120,6 +266,7 @@
                         cols = data[i].length;
                     }
                 }
+                options.cols = cols;
             }else{
                 cols = options.cols;
             }
@@ -130,12 +277,48 @@
          */
         this.addRow = function(rowData){
             var row = $('<TR>');
+            if(greed.options.get('showRowNumbers')){
+                var headerCell = $('<TH>');
+                var rowNumber = table.find('TR').length;
+                if(!greed.options.get('showColumnCaptions')){
+                    rowNumber += 1;
+                }
+                headerCell.text(rowNumber);
+                row.append(headerCell);
+            }
             for(var i=0; i < cols; i++){
                 var cell = $('<TD>');
                 if(typeof(rowData) == 'undefined'){
                     rowData = [];
                 }
                 cell.text(typeof(rowData[i]) != 'undefined' ? rowData[i] : '');
+                row.append(cell);
+            }
+            table.append(row);
+        }
+
+        /**
+         * Gets cell by coordinates
+         */
+        this.getCell = function(col, row){
+            if(greed.options.get('showColumnCaptions')){
+                row++;
+            }
+            return table.find('TR:eq(' + row +') TD:eq(' + col + ')');
+        }
+
+        /**
+         * Columns caption row
+         */
+        var addColumnCaptionsRow = function(){
+            var row = $('<TR>');
+            if(greed.options.get('showRowNumbers')){
+                var headerCell = $('<TH>');
+                row.append(headerCell);
+            }
+            for(var i=0; i < cols; i++){
+                var cell = $('<TH>');
+                cell.text(greed.columns.getCaption(i));
                 row.append(cell);
             }
             table.append(row);
@@ -154,18 +337,22 @@
             // + greed.data
             // + greed.history
 
-            greed.options = new Greed.options(greed, options);
-            greed.table = new Greed.table(greed, data);
+            greed.options   = new Greed.options(greed, options);
+            greed.columns   = new Greed.columns(greed);
+            greed.table     = new Greed.table(greed, data);
+            greed.cursor    = new Greed.cursor(greed);
 
-            // + greed.cursor
             // + greed.selection
             // + greed.clipboard
             // + greed.events
-            // + greed.headers
 
             // Initialize table
             greed.table.init();
             this[0].greed = greed;
+
+            greed.cursor.init();
+
+            Greed.instances.push(greed);
 
             /*
             if(this[0].nodeName == 'TABLE'){
